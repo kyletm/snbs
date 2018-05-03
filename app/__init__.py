b4655fefd055e8ccbdfd371bf42c38e748f2b551
kyletm <newportkyle@gmail.com>
2018-05-03 01:32:20 -0400

    fix: vim troubles and regenerated alembic migrations

--- app/__init__.py
+++ app/__init__.py
@@ -43,8 +43,8 @@ if not app.debug:
     else:
         if not os.path.exists('logs'):
             os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/microblog.log', maxbytes=10240, backupCount=10)
-        file_handler.setformatter(logging.formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
 
