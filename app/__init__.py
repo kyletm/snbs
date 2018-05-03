00c84b06f0bf6b15efc88620fc14cf874daccea6
kyletm <newportkyle@gmail.com>
2018-05-03 01:22:36 -0400

    fix: fixed imports

--- app/__init__.py
+++ app/__init__.py
@@ -39,12 +39,11 @@ if not app.debug:
     if app.config['LOG_TO_STDOUT']:
         stream_handler = logging.StreamHandler()
         stream_handler.setLevel(logging.INFO)
-        api.logger.addHandler(stream_handler)
+        app.logger.addHandler(stream_handler)
     else:
         if not os.path.exists('logs'):
             os.mkdir('logs')
-        file_handler = rotatingfilehandler('logs/microblog.log', maxbytes=10240,
-                       backupcount=10)
+        file_handler = RotatingFileHandler('logs/microblog.log', maxbytes=10240, backupcount=10)
         file_handler.setformatter(logging.formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
